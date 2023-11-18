6d53f15b952ce1da86cd5a5c4832305c25b1e3e9
ASUS-PC <ASUS-PC@RIGOBERTO>
2023-11-17 23:10:42 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Practica_1_Unidad_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtNumero1 = new TextBox();
+            txtNumero2 = new TextBox();
+            txtResultado = new TextBox();
+            btnSumar = new Button();
+            btnRestar = new Button();
+            btnMultiplicar = new Button();
+            btnDividir = new Button();
+            btnLimpiar = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(44, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Numero 1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(44, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Numero 2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(53, 241);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Resultado";
+            // 
+            // txtNumero1
+            // 
+            txtNumero1.Location = new Point(123, 21);
+            txtNumero1.Name = "txtNumero1";
+            txtNumero1.Size = new Size(100, 23);
+            txtNumero1.TabIndex = 3;
+            // 
+            // txtNumero2
+            // 
+            txtNumero2.Location = new Point(123, 76);
+            txtNumero2.Name = "txtNumero2";
+            txtNumero2.Size = new Size(100, 23);
+            txtNumero2.TabIndex = 4;
+            // 
+            // txtResultado
+            // 
+            txtResultado.Location = new Point(123, 233);
+            txtResultado.Name = "txtResultado";
+            txtResultado.Size = new Size(100, 23);
+            txtResultado.TabIndex = 5;
+            // 
+            // btnSumar
+            // 
+            btnSumar.Location = new Point(343, 17);
+            btnSumar.Name = "btnSumar";
+            btnSumar.Size = new Size(75, 23);
+            btnSumar.TabIndex = 6;
+            btnSumar.Text = "Sumar";
+            btnSumar.UseVisualStyleBackColor = true;
+            btnSumar.Click += btnSumar_Click;
+            // 
+            // btnRestar
+            // 
+            btnRestar.Location = new Point(343, 46);
+            btnRestar.Name = "btnRestar";
+            btnRestar.Size = new Size(75, 23);
+            btnRestar.TabIndex = 7;
+            btnRestar.Text = "Restar";
+            btnRestar.UseVisualStyleBackColor = true;
+            btnRestar.Click += btnRestar_Click;
+            // 
+            // btnMultiplicar
+            // 
+            btnMultiplicar.Location = new Point(343, 76);
+            btnMultiplicar.Name = "btnMultiplicar";
+            btnMultiplicar.Size = new Size(75, 23);
+            btnMultiplicar.TabIndex = 8;
+            btnMultiplicar.Text = "Multiplicar";
+            btnMultiplicar.UseVisualStyleBackColor = true;
+            btnMultiplicar.Click += btnMultiplicar_Click;
+            // 
+            // btnDividir
+            // 
+            btnDividir.Location = new Point(343, 105);
+            btnDividir.Name = "btnDividir";
+            btnDividir.Size = new Size(75, 23);
+            btnDividir.TabIndex = 9;
+            btnDividir.Text = "Dividir";
+            btnDividir.UseVisualStyleBackColor = true;
+            btnDividir.Click += btnDividir_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new Point(37, 129);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(75, 23);
+            btnLimpiar.TabIndex = 10;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(256, 232);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 23);
+            button1.TabIndex = 11;
+            button1.Text = "Propiedades";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnDividir);
+            Controls.Add(btnMultiplicar);
+            Controls.Add(btnRestar);
+            Controls.Add(btnSumar);
+            Controls.Add(txtResultado);
+            Controls.Add(txtNumero2);
+            Controls.Add(txtNumero1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtNumero1;
+        private TextBox txtNumero2;
+        private TextBox txtResultado;
+        private Button btnSumar;
+        private Button btnRestar;
+        private Button btnMultiplicar;
+        private Button btnDividir;
+        private Button btnLimpiar;
+        private Button button1;
+    }
+}
